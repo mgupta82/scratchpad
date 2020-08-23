a52486be9da5adc2ac138dd1b4203259676e6e28
Mukesh Gupta <mukeshsgupta@gmail.com>
2020-08-23 19:49:01 +1000

    Class based component using state and getting user geolocation

---
+++ jsxclass/src/SeasonDisplay.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const SeasonDisplay = (props) => {
+    return (
+        <div>Latitude : {props.lat}</div>
+    );
+};
+
+export default SeasonDisplay;
--- jsxclass/src/index.js
+++ jsxclass/src/index.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
 
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {lat : null};
 
-const App = () => {
-    return (
-        <div>
-            Hi there!
-        </div>
+    window.navigator.geolocation.getCurrentPosition(
+      position => this.setState({lat:position.coords.latitude}),
+      err => console.log(err)
     );
-};
+
+  }
+
+  render() {
+    return (
+      <SeasonDisplay lat={this.state.lat}/>
+    );
+  }
+  
+}
 
 ReactDOM.render(<App/>,document.querySelector('#root'));
