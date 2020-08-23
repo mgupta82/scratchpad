0b18c87561baa376cb21ba83018d9941afc696ba
Mukesh Gupta <mukeshsgupta@gmail.com>
2020-08-23 19:19:33 +1000

    Approval Card using  props.children

--- jsxfunc/src/index.js
+++ jsxfunc/src/index.js
@@ -2,12 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import faker from 'faker';
 import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 
 const App = () => {
     return (
         <div className="ui container comments">
-            <CommentDetail avatar={faker.image.avatar()} author="Sam" time="2 min ago" comment="Awesome"/>
             <CommentDetail avatar={faker.image.avatar()} author="Tony" time="1 min ago" comment="Rock on"/>
+            <ApprovalCard>
+                <CommentDetail avatar={faker.image.avatar()} author="Sam" time="2 min ago" comment="Awesome"/>
+            </ApprovalCard>
+            <ApprovalCard>
+                Are you Sure?
+            </ApprovalCard>
+            
+            
         </div>
     );
 };
