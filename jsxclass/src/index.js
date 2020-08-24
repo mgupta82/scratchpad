447ba3a21ed92a79ad0d5cfea8e52e765ec41d35
Mukesh Gupta <mukeshsgupta@gmail.com>
2020-08-24 14:28:49 +1000

    Use component lifecycle methods

--- jsxclass/src/index.js
+++ jsxclass/src/index.js
@@ -6,12 +6,13 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {lat : null};
+  }
 
+  componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
       position => this.setState({lat:position.coords.latitude}),
       err => console.log(err)
     );
-
   }
 
   render() {
