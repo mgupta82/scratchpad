1b20e434953e9b20841be8f73dec1f70aa5b61b3
Mukesh Gupta <mukeshsgupta@gmail.com>
2020-08-28 09:31:42 +1000

    define types for  literal, function, interface , class

--- tsback/index.ts
+++ tsback/index.ts
@@ -1,7 +1,45 @@
 import axios from 'axios';
 
+//define type of scope variable
+let apples:number;
+apples = 5;
+
+//define type of object literal
+let coordiates : {x:number,y:number} = {
+    x: 5,
+    y : 10
+}
+
+const json = '{"x":10,"y":"30"}';
+
+coordiates = JSON.parse(json);
+console.log('coordinates: ' + coordiates.x+'-'+coordiates.y);
+
+//define object of type class
+class Car {
+
+}
+
+let car: Car = new Car();
+
+//Define type for interface
+interface Todo {
+    id: number;
+    title: string;
+    completed: boolean;
+}
+
 const url = "https://jsonplaceholder.typicode.com/todos/1";
 
 axios.get(url).then(response =>{
-    console.log(response.data);
-})
+    const todo = response.data as Todo;
+    logTodo(todo.id,todo.title,todo.completed);
+});
+
+//define type of function paramters and return type
+function logTodo(id: number,title: string,completed: boolean):void {
+    console.log(`
+        The Todo with ID: ${id}
+        Has a title of: ${title}
+        Is it completed: ${completed}`);
+}
